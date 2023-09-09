89030fb603ad52302fa9647934c240d74f459bda
Varun <varun.jajoo18@gmail.com>
2023-09-10 00:42:27 +0530

    commit

--- src/components/GG.jsx
+++ src/components/GG.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-
+import {HiTrophy} from "react-icons/hi2"
+import trophy from "../images/44124.jpg"
 export default function GG() {
   return (
-    <div></div>
+    <div ><img className='trophy' src={trophy} alt="" /></div>
   )
 }
