5a21bab67cc94faae817c42e98409c982c2adcb6
Varun <varun.jajoo18@gmail.com>
2023-09-11 01:47:02 +0530

    commit

--- src/components/Stockchart.jsx
+++ src/components/Stockchart.jsx
@@ -1,58 +1,36 @@
-// TradingViewWidget.jsx
+import React from 'react';
+import { Chart } from 'react-charts';
 
-import React, { useEffect, useRef } from 'react';
+export default function StockChart() {
+  // Sample chart data
+  const data = React.useMemo(
+    () => [
+      {
+        label: 'Stock Prices',
+        data: [
+          [1, 10],
+          [2, 20],
+          [3, 15],
+          [4, 25],
+          // Add more data points as needed
+        ],
+      },
+    ],
+    []
+  );
 
-let tvScriptLoadingPromise;
-
-export default function TradingViewWidget() {
-  const onLoadScriptRef = useRef();
-
-  useEffect(
-    () => {
-      onLoadScriptRef.current = createWidget;
-
-      if (!tvScriptLoadingPromise) {
-        tvScriptLoadingPromise = new Promise((resolve) => {
-          const script = document.createElement('script');
-          script.id = 'tradingview-widget-loading-script';
-          script.src = 'https://s3.tradingview.com/tv.js';
-          script.type = 'text/javascript';
-          script.onload = resolve;
-
-          document.head.appendChild(script);
-        });
-      }
-
-      tvScriptLoadingPromise.then(() => onLoadScriptRef.current && onLoadScriptRef.current());
-
-      return () => onLoadScriptRef.current = null;
-
-      function createWidget() {
-        if (document.getElementById('tradingview_cf744') && 'TradingView' in window) {
-          new window.TradingView.widget({
-            autosize: true,
-            symbol: "NASDAQ:AAPL",
-            interval: "D",
-            timezone: "Etc/UTC",
-            theme: "light",
-            style: "1",
-            locale: "en",
-            enable_publishing: false,
-            allow_symbol_change: true,
-            container_id: "tradingview_cf744"
-          });
-        }
-      }
-    },
+  // Sample chart configuration
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'linear', position: 'bottom' },
+      { type: 'linear', position: 'left' },
+    ],
     []
   );
 
   return (
-    <div className='tradingview-widget-container'>
-      <div id='tradingview_cf744' />
-      <div className="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a>
-      </div>
-    </div>
-  );
+    <div className="chart-container" style={{height:"90vh"}}>
+      <Chart data={data} axes={axes} />
+    </div>
+  );
 }
--- src/screens/Stock.jsx
+++ src/screens/Stock.jsx
@@ -1,94 +1,55 @@
 import React, { useState, useEffect } from 'react';
-// import './main.css'; // Import your CSS file
-import TradingViewWidget from 'react-tradingview-widget';
+import "../css/stock.css";
+import StockChart from '../components/Stockchart';
+import axios from 'axios';
 
-function Stock() {
-  const [search, setSearch] = useState('AMID');
-  const [stockInfo, setStockInfo] = useState({});
-  const [indices, setIndices] = useState({});
-  const [showSearchBar, setShowSearchBar] = useState(false);
-  const [showChart, setShowChart] = useState(false);
-  const lastUpdated = new Date().toLocaleString();
+export default function Stock() {
+  const [stockdata, setstockData] = useState([]);
+  const TOKEN = "cjutf79r01qlmkvcr39gcjutf79r01qlmkvcr3a0";
+  const urlbase = "https://finnhub.io/api/v1/quote";
 
   useEffect(() => {
-    // Fetch stock information and indices on component mount
-    getStockInfo();
-    getIndices();
-    getStockChart();
-    
-    // Set up an interval to periodically fetch stock information
-    const interval = setInterval(getStockInfo, 10000);
-    
-    // Clear the interval when the component unmounts
-    return () => clearInterval(interval);
+    const stocklist = ["AAPL", "MSFT", "TSLA", "FB", "BABA", "UBER", "DIS", "SBUX"];
+    const promises = stocklist.map((stock) =>
+      getstockdata(stock).then((res) => ({ name: stock, ...res.data }))
+    );
+
+    Promise.all(promises)
+      .then((data) => {
+        console.log(data);
+        setstockData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching stock data", error.message);
+      });
   }, []);
 
-  const getStockInfo = () => {
-    // Fetch stock data from the API, similar to your original code
-    // Update the stockInfo state with the fetched data
-  };
-
-  const getIndices = () => {
-    // Fetch indices data from the API, similar to your original code
-    // Update the indices state with the fetched data
-  };
-
-  const getStockChart = () => {
-    // Initialize the TradingView widget here, similar to your original code
-  };
-
-  const handleSearchClick = () => {
-    setShowSearchBar(!showSearchBar);
-  };
-
-  const handleViewChartClick = () => {
-    setShowChart(!showChart);
-  };
-
-  const handleSearchKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      setSearch(e.target.value);
-      getStockInfo();
-      getStockChart();
-    }
+  const getstockdata = (stock) => {
+    return axios
+      .get(`${urlbase}?symbol=${stock}&token=${TOKEN}`)
+      .catch((error) => {
+        console.error("Error fetching stock data", error.message);
+      });
   };
 
   return (
-    <div className="container">
-      <input
-        type="text"
-        value={search}
-        className={`searchbar ${showSearchBar ? '' : 'hidden'}`}
-        placeholder="Enter stock symbol"
-        onKeyPress={handleSearchKeyPress}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <div className="header">
-        {/* Display stock information here using stockInfo state */}
-      </div>
-      <hr />
-      <a href="#" onClick={handleSearchClick}>
-        Search for stocks
-      </a>
-      <div className="indices">
-        {/* Display indices information here using indices state */}
-      </div>
-      <a href="#" onClick={handleViewChartClick}>
-        View chart
-      </a>
-      <div id="stockChart" className={showChart ? '' : 'hidden'}>
-        {/* Use the TradingViewWidget component here */}
-        <TradingViewWidget
-          symbol={['Google', search]}
-          // Other props for the TradingView widget
-        />
-      </div>
-      <div className="lastUpdated">Last updated: {lastUpdated}</div>
-      <div className="closeTime">
-        Trading Window Closes: {/* Display close time from stockInfo */}
+    <div className='stock-container'>
+      <div className="title">Gear Up Your Stocks</div>
+      <div className="stock-body">
+        <div className="charts">
+          <StockChart  />
+        </div>
+        <div className="stocks">
+          <ul>
+            {stockdata.map((stock) => (
+              <li key={stock.name}>
+                <strong>{stock.name}: </strong>
+                Price: {stock.c}, Open: {stock.o}, High: {stock.h}, Low: {stock.l}, Volume: {stock.v}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
 }
-
-export default Stock;
