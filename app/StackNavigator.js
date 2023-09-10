90083cf034636d7643b676dd44aadc1030e90de6
Shreyans jain <shreyansjain1603@gmail.com>
2023-09-10 23:59:30 +0530

    commit

--- app/StackNavigator.js
+++ app/StackNavigator.js
@@ -1,7 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const StackNavigator = () => {
+    const Tab = createBottomTabNavigator();
+    const Stack = createStackNavigator();
+
   return (
     <View>
       <Text>StackNavigator</Text>
