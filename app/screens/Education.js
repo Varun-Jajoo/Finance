4f8ddabe0bc4f6c868409afec621855cb7df3b4f
Varun <varun.jajoo18@gmail.com>
2023-09-11 22:33:22 +0530

    commit

--- app/screens/Education.js
+++ app/screens/Education.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { ScrollView,View, Text, Pressable, Image, ProgressBarAndroid, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import * as Progress from "react-native-progress"
 const Education = () => {
   return (
-    <ScrollView>
+    <ScrollView style={{ display: "flex",backgroundColor:"black",position:"relative" }}>
+      <Image source={require("../assets/school.png")} style={{position:"absolute",height:300,width:400,top:0,left:0,backgroundColor:"#F1C93B"}}/>
+     
       <View
         style={{
           display: "flex",
           justifyContent: "space-around",
           alignItems: "center",
           flexDirection: "row",
-          paddingTop: 80,
+          paddingTop: 240,
+          
         }}
       >
         <Pressable
@@ -19,11 +23,10 @@ const Education = () => {
             flexDirection:"row",
             justifyContent: "center",
             alignItems: "center",
-            backgroundColor: "#d5ecf9",
+            backgroundColor: "#98bc62",
             height: 100,
             width: 160,
-            borderWidth: 4,
-            borderBlockColor: "rgb(15, 37, 145)",
+           
             borderRadius: 20,
           }}
         >
@@ -38,11 +41,10 @@ const Education = () => {
             flexDirection:"row",
             justifyContent: "center",
             alignItems: "center",
-            backgroundColor: "#ffcffa",
+            backgroundColor: "#98bc62",
             height: 100,
             width: 160,
-            borderWidth: 4,
-            borderBlockColor: "rgb(120, 21, 120)",
+           
             borderRadius: 20,
           }}
         >
@@ -52,7 +54,7 @@ const Education = () => {
         </Pressable>
       </View>
       <View style={{flexDirection:'row',display:'flex',justifyContent:"space-evenly"}}>
-      <Text style={{fontWeight:"bold",fontSize:20,paddingTop:18}}>Courses</Text>
+      <Text style={{fontWeight:"bold",color:"white",fontSize:20,paddingTop:18}}>Courses</Text>
       <Text style={{fontWeight:"600",fontSize:15,textDecorationLine:"underline",color:"grey",paddingTop:20,marginLeft:120}}>See all</Text>
       </View>
       <View
@@ -64,11 +66,11 @@ const Education = () => {
           justifyContent: "center",
           alignItems: "center",
           color: "white",
+          paddingBottom:40
         }}
       >
-        <LinearGradient
-        colors={['rgba(192,104,233,1)', 'rgba(106,81,229,1)']}
-        start={{x:1,y:0}}
+        <Pressable
+        
           style={{
             display: "flex",
             flexDirection: "row",
@@ -76,7 +78,7 @@ const Education = () => {
             alignItems: "center",
             height: 120,
             width: 350,
-            backgroundColor: "rgba(106,81,229,1)",
+            backgroundColor: "#222222",
             borderRadius: 35,
             elevation: 10,
           }}
@@ -84,18 +86,11 @@ const Education = () => {
           <Image source={require('../assets/3d-calculator_10473465.png')} style={{ height: 80, width: 80 }} />
           <View style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
            <Text style={{ color: "white", fontWeight: "900", fontSize: 20, marginLeft: 20 ,marginRight:20}}>Basics of Finance</Text>
-           <ProgressBarAndroid
-            styleAttr="Horizontal"
-            indeterminate={false}
-            progress={0.75}
-             style={{width:125,marginTop:20,marginLeft:20}}
-           
-          />
+           <Progress.Bar borderColor='transparent' unfilledColor='white' color='rgb(59,198,84)' progress={0.3} width={150} height={10} borderRadius={20} style={{marginTop:20}}/>
           </View>
-        </LinearGradient>
-        <LinearGradient
-        colors={["rgba(104,157,233,1) ","#457fe5"]}
-        start={{x:0,y:1}}
+        </Pressable>
+        <Pressable
+       
           style={{
             display: "flex",
             flexDirection: "row",
@@ -103,36 +98,7 @@ const Education = () => {
             alignItems: "center",
             height: 120,
             width: 350,
-            backgroundColor: "dodgerblue",
-            borderRadius: 35,
-            elevation: 10,
-          }}
-        >
-          <Image source={require('../assets/piggy-bank_1511168.png')} style={{ height: 80, width: 80, margin: 8 }} />
-          <View style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-            <Text style={{ color: "white", fontWeight: "900", fontSize: 20 }}>Entreprenuship Guide</Text>
-            <ProgressBarAndroid
-            styleAttr="Horizontal"
-            indeterminate={false}
-            progress={0.75}
-             style={{width:125,height:10,margin:20}}
-           
-          />
-          </View>
-        </LinearGradient>
-
-        <LinearGradient
-        colors={['rgba(233,179,104,1) ', 'rgba(210,65,65,1)']}
-        start={{ x: 0, y: 0 }} 
-        end={{ x: 0.5, y: 1 }} 
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            height: 120,
-            width: 350,
-            backgroundColor: "rgba(210,65,65,1) ",
+            backgroundColor: "#222222",
             borderRadius: 35,
             elevation: 10,
           }}
@@ -141,16 +107,32 @@ const Education = () => {
           <View style={{ display: "flex", flexDirection: "column" ,justifyContent: "center", alignItems: "center"}}>
             <Text style={{ color: "white", fontWeight: "900", fontSize: 20, marginRight: 30, textAlign: "left" }}>Scam Prevention</Text>
           
-          <ProgressBarAndroid
-            styleAttr="Horizontal"
-            indeterminate={false}
-            progress={0.75}
-             style={{width:125,margin:20}}
-             
-           
-          />
+            <Progress.Bar borderColor='transparent' unfilledColor='white' color='rgb(59,198,84)' progress={0.3} width={150} height={10}  borderRadius={20} style={{marginTop:20}}/>
            </View>
-        </LinearGradient>
+        </Pressable>
+        <Pressable
+        
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            height: 120,
+            width: 350,
+            backgroundColor: "#222222",
+            borderRadius: 35,
+            elevation: 10,
+            
+          }}
+        >
+          <Image source={require('../assets/piggy-bank_1511168.png')} style={{ height: 80, width: 80, margin: 8 }} />
+          <View style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ color: "white", fontWeight: "900", fontSize: 20 }}>Entreprenuship Guide</Text>
+            <Progress.Bar borderColor='transparent' unfilledColor='white' color='rgb(59,198,84)' progress={0.3} width={150} height={10}  borderRadius={20} style={{marginTop:20}}/>
+          </View>
+        </Pressable>
+
+        
       </View>
     </ScrollView>
   );
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,14 +1,146 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ProgressBarAndroid,
+  ImageBackground,
+} from "react-native";
+import React from "react";
+import * as Progress from "react-native-progress";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <ScrollView style={{ display: "flex",backgroundColor:"#1b1b1b",position:"relative" }}>
+      <Image source={require("../assets/Savings-cuate.png")} style={{position:"absolute",height:300,width:400,top:0,left:0,backgroundColor:"#2b6747"}}/>
+      <Pressable style={styles.savetop}>
+        <Image
+          source={require("../assets/tugrik_9328452.png")}
+          style={{ height: 80, width: 80, marginRight: 20 }}
+        />
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+          }}
+        >
+          <Text
+            style={{
+              color: "#1b1b1b",
+              fontWeight: "900",
+              fontSize: 20,
+              marginRight: 30,
+              textAlign: "left",
+            }}
+          >
+            Add a Saving Goal
+          </Text>
+          <Text style={{ textAlign: "center" ,color:"#1b1b1b"}}>We will keep a report! </Text>
+          {/* <ProgressBarAndroid
+            styleAttr="Horizontal"
+            indeterminate={false}
+            progress={0.75}
+             style={{width:125,margin:20}}
+             
+           
+          /> */}
+        </View>
+      </Pressable>
+      <Pressable style={styles.save1}>
+        <ImageBackground
+        >
+        <View
+          style={{ display: "flex", flexDirection: "column", marginTop: 20 }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            {/* <Image
+              source={require("")}
+              style={{ height: 60, width: 60, marginRight: 20 }}
+            /> */}
+            <View>
+              <Text
+                style={{
+                  color: "black",
+                  fontWeight: "900",
+                  fontSize: 20,
+                  marginRight: 30,
+                  textAlign: "left",
+                }}
+              >
+                Buying a Phone
+              </Text>
+              <Text style={{}}>Trust the process </Text>
+            </View>
+          </View>
 
-export default Home
+          <Progress.Bar
+            borderColor="transparent"
+            animationType="spring"
+            unfilledColor="white"
+            color="rgb(59,198,84)"
+            progress={0.3}
+            width={255}
+            height={18}
+            borderRadius={20}
+            style={{ marginTop: 20, elevation: 10 }}
+          />
+          <Text
+            style={{
+              color: "black",
+              fontWeight: "900",
+              fontSize: 16,
+              marginRight: 30,
+              textAlign: "left",
+              marginTop: 20,
+            }}
+          >
+            Balance
+          </Text>
+          <Text style={{}}>₹ 3000 / ₹ 10000 </Text>
+        </View>
+        </ImageBackground>
+      </Pressable>
+    </ScrollView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Home;
+
+const styles = StyleSheet.create({
+  savetop: {
+    height: 100,
+    width: 350,
+    backgroundColor: "#98bc62",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 24,
+    
+    borderRadius: 20,
+    elevation: 20,
+    marginTop:250
+  },
+  save1: {
+    height: 220,
+    width: 350,
+    backgroundColor: "#F1C93B",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "baseline",
+    margin: 24,
+    borderRadius: 20,
+    elevation: 10,
+  },
+});
