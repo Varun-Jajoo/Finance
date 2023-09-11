f6b7e1e303c795a1de32196ae18cf052acbb0d63
Kashik Sredharan <kashik.sredharan@gmail.com>
2023-09-11 22:24:35 +0530

    backend

--- backend/mongo.js
+++ backend/mongo.js
@@ -24,27 +24,27 @@ const newSchema = new mongoose.Schema({
         required:true
     },
     depend:{
-        type:Integer,
+        type:Number,
         required:true
     },
     misc_exp:{
-        type:Double,
+        type:Number,
         required:false
     },
     lst_mnth_sav:{
-        type:Double,
+        type:Number,
         required:false
     },
     ann_inc:{
-        type:Double,
+        type:Number,
         required:false
     },
     mnt_cst:{
-        type:Double,
+        type:Number,
         required:false
     },
     rent_emi:{
-        type:Double,
+        type:Number,
         required:false
     },
     area:{
@@ -54,8 +54,141 @@ const newSchema = new mongoose.Schema({
     age:{
         type:String,
         required:true
+    },
+    coins:{
+        type:Number,
+        required:false
+    },
+    bof_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    bof_lvl_ls_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    bof_lvl_tq_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    bof_lvl_wt_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    bof_lvl_vt_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    eg_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    eg_lvl_ls_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    eg_lvl_tq_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    eg_lvl_wt_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    eg_lvl_vt_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    sp_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    sp_lvl_ls_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    sp_lvl_tq_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    sp_lvl_wt_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    sp_lvl_vt_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    fl_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    fl_lvl_ls_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    fl_lvl_tq_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    fl_lvl_wt_lvl:{
+        type:Number,
+        max: 4,
+        required:false
+    },
+    fl_lvl_vt_lvl:{
+        type:Number,
+        max: 4,
+        required:false
     }
-})
-const collection = mongoose.model("user_Data",newSchema)
 
-module.exports=collection
+})
+const sch_stock =new mongoose.Schema({
+	tstmp_pur:{
+		type:Timestamp
+	},
+	curr_price:{
+		type:Number
+	}
+
+}
+) 
+const sch_mnt_cst = new mongoose.Schema({
+	avg_rent:{
+		type:Number,
+		required:true
+	},
+	dep:{
+		type:Number,
+		required:true
+	},
+	avg_mtn_cst:{
+		type:Number
+	}
+
+})
+const userData = mongoose.model("user_Data",usr_data)
+const stockData = mongoose.model("Stock_stamp",sch_stock)
+const PredictorData = mongoose.model("Mtn_cst_prediction",sch_mnt_cst)
+
+module.exports=userData
+module.exports=stockData
+module.exports= PredictorData
