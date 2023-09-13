ecd5760f30e163f9a53e27749ad0fd737b814c1a
Varun <varun.jajoo18@gmail.com>
2023-09-13 20:43:37 +0530

    commit

--- app/screens/Community.js
+++ app/screens/Community.js
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,ScrollView} from 'react-native'
 import React from 'react'
 import Popup from "../components/Popup"
+import Videotut from '../components/Videotut'
+import ExpensesLanding from '../components/ExpensesLanding'
+
 const Community = () => {
   return (
-    <View>
-      <Popup/>
-    </View>
+    <ScrollView>
+     <ExpensesLanding/>
+    </ScrollView>
   )
 }
 
